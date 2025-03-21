d89c5bad71d26a3c8e2d825a0edf8d859761fff6
Felipe <feltista07123@gmail.com>
2025-03-21 14:47:07 -0300

    feat: implementa lógica para puxar os dados do usuario e verificar se o token é válido

--- src/app/api/src/controllers/authCheckToken.ts
+++ src/app/api/src/controllers/authCheckToken.ts
@@ -1,40 +1,47 @@
-import { useEffect } from 'react';
-import Cookies from 'js-cookie';
-import { useRouter } from 'next/navigation';
+import { useEffect, useState } from "react";
+import Cookies from "js-cookie";
+import { useRouter } from "next/navigation";
 
-// Hook personalizado para verificar a validade do token
+// Hook personalizado para verificar a validade do token e obter dados do usuário
 export const useCheckTokenValidity = () => {
+  const [user, setUser] = useState<{ name: string; username: string } | null>(null);
+  const [loading, setLoading] = useState(true); // Para controlar o estado de carregamento
   const router = useRouter();
 
   useEffect(() => {
-    const token = Cookies.get('token');
+    const token = Cookies.get("token");
 
     if (token) {
       const verifyToken = async () => {
         try {
-          const response = await fetch('http://localhost:8000/users/me', {
-            method: 'GET',
+          const response = await fetch("http://localhost:8000/users/me", {
+            method: "GET",
             headers: {
-              'Authorization': `Bearer ${token}`,
+              Authorization: `Bearer ${token}`,
             },
           });
 
-          if (!response.ok) {
-            // Se a resposta for negativa (token expirado), limpa o cookie e redireciona para login
-            Cookies.remove('token');
-            router.push('/login');
+          if (response.ok) {
+            const data = await response.json();
+            setUser({ name: data.name, username: data.username });
+          } else {
+            Cookies.remove("token");
+            router.push("/login");
           }
         } catch (error) {
           console.error("Erro ao validar token:", error);
-          Cookies.remove('token');
-          router.push('/login');
+          Cookies.remove("token");
+          router.push("/login");
+        } finally {
+          setLoading(false);
         }
       };
 
       verifyToken();
     } else {
-      // Se o token não existir, redireciona para login
-      router.push('/login');
+      router.push("/login");
     }
   }, [router]);
+
+  return { user, loading };
 };
