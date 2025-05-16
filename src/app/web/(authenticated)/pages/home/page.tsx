d5134c0e0a021fadaf3559c30ac7ed119eafc59b
thelipesousa <feltista07123@gmail.com>
2025-05-16 17:34:15 -0300

    chore: chama verificação de token e deixa invisivel a scrollbar

--- src/app/web/(authenticated)/pages/home/page.tsx
+++ src/app/web/(authenticated)/pages/home/page.tsx
@@ -4,14 +4,18 @@ import Sidebar from "@/components/ui/sidebar"
 import RightSidebar from "@/components/ui/Rightsidebar"
 import Header from "@/components/ui/header"
 import PostList from "./components/post-list"
+import { useCheckTokenValidity } from "@/app/api/src/controllers/authCheckToken"
 
 export default function Home() {
+
+  useCheckTokenValidity();
+
   return (
-    <div className="min-h-screen bg-gray-100 text-[#161616]">
+    <div className="min-h-screen bg-gray-100 text-[#161616] scrollbar-hide overflow-hidden">
       <Sidebar variant="static" />
-      <div className="ml-64">
+      <div className="ml-64 scrollbar-hide overflow-auto h-screen">
         <Header />
-        <main className="flex flex-1 pt-16 no-scrollbar">
+        <main className="flex flex-1 pt-16 scrollbar-hide overflow-auto">
           <PostList />
           <RightSidebar />
         </main>
