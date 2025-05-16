3b6d4865c38a2b1a86c84cfb2721dd44729b1b16
thelipesousa <feltista07123@gmail.com>
2025-05-16 17:36:49 -0300

    chore: ajusta posição da Searchbar

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -15,13 +15,13 @@ export default function Header() {
 
   return (
     <header className="flex items-center font-manrope justify-between p-4 bg-gray-100 border-b border-[#e0e0e0] h-16 fixed top-0 left-[16rem] right-0 z-10">
-      <div className="w-full max-w-xl">
+      <div className="w-full max-w-xl ml-86">
         <div className="relative">
           <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-[#525252] h-4 w-4" />
           <input
             type="text"
             placeholder="Pesquisar"
-            className="w-full pl-10 pr-4 py-2 border border-[#e0e0e0] focus:outline-none focus:border-[#0f62fe]"
+            className="w-[675px] pl-10 pr-4 py-2 border border-[#e0e0e0] focus:outline-none focus:border-[#0f62fe]"
           />
         </div>
       </div>
